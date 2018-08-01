89ded8dfc365a7ae85f40e0062644ea3f7b16c81
nivedita <nivedita@10.0.0.62>
2018-08-01 14:31:36 -0700

    Array rotation with given order

--- src/ArrayRotate.java
+++ src/ArrayRotate.java
@@ -2,31 +2,36 @@
 public class ArrayRotate {
 
 	
-	public static int[] rotate(int[] arr, int order) {
-		int i, j, temp;
+	public static int[] rotatearray(int[] arr, int order) {
+		//int i, j, temp;
 		if (arr == null || order < 0) {
 		    throw new IllegalArgumentException("Illegal argument!");
 		}
 	 
-		for (i = 0; i < order; i++) {
-			for (j = arr.length - 1; j > 0; j--) {
-				temp = arr[j];
+		for (int i = 0; i < order; i++) {
+			for (int j = arr.length - 1; j > 0; j--) {
+				int temp = arr[j];
 				arr[j] = arr[j - 1];
 				arr[j - 1] = temp;
-				System.out.println(temp);			
 			}
-			
+		
 		}
 		return arr;
 
 	}
+	public void printArray(int arr[], int n) 
+    {
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayRotate ar = new ArrayRotate();
 		int[] array = {0,1,2,3,4,5,6,7};
-		int[] result = ArrayRotate.rotate(array, 3);
-		System.out.println(result);	
-		
+		ar.rotatearray(array, 3);
+		ar.printArray(array, 8);
+		//System.out.println( result);
 		
 		
 
